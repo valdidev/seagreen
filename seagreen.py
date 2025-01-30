02b1783a7c1e2685089a9435c99c546582a59d37
Fernando Valdivielso <iamvaldidev@gmail.com>
2025-01-30 19:53:01 +0100

    agrupar por horas en diccionario

--- seagreen.py
+++ seagreen.py
@@ -1,12 +1,17 @@
 archivo = open("darkslateblue-access.log", "r")
 
-lineas = archivo.readlines()
+diccionario = {}
 
+lineas = archivo.readlines()
 for linea in lineas:
     inicio = linea.find('[')
     final = linea.find(']')
     tiempo = linea[inicio+1:final]
     partes = tiempo.split(':')
     hora = partes[1]
-    print(hora)
+    if hora not in diccionario:
+        diccionario[hora] = 1
+    else:
+        diccionario[hora] += 1
+print(diccionario)
 
