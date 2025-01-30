830595081875cbedb312c9c1370e7e541b814f20
Fernando Valdivielso <iamvaldidev@gmail.com>
2025-01-30 19:58:20 +0100

    sacar accesos / hora en gráfica

--- seagreen.py
+++ seagreen.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+
 archivo = open("darkslateblue-access.log", "r")
 
 diccionario = {}
@@ -16,6 +18,13 @@ for linea in lineas:
 
 horas = sorted(diccionario.keys())
 numero = [diccionario[hora] for hora in horas]
-print(horas)
-print(numero)
 
+# gráfica
+plt.figure(figsize=(10, 5))
+plt.bar(horas, numero)
+plt.xlabel('Hora')
+plt.ylabel('Número de accesos')
+plt.title('Número de accesos por hora')
+plt.xticks(rotation=45)
+plt.tight_layout()
+plt.show()
