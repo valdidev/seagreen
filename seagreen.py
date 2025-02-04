85529f781f93dc46fdd0cb3b778675b84d0f6bcb
Fernando Valdivielso <iamvaldidev@gmail.com>
2025-02-04 20:27:54 +0100

    cambiar ruta para server

--- seagreen.py
+++ seagreen.py
@@ -2,7 +2,8 @@ import os
 from funciones.registros_por_hora import registros_por_hora
 
     
-carpeta = "C:/xampp/htdocs/seagreen"
+# carpeta = "C:/xampp/htdocs/seagreen"
+carpeta = "/var/log/apache2/"
 
 for root, dirs, files in os.walk(carpeta):
     for file in files:
